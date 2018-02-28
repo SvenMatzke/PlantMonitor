f92c63cb5384009ce5c31d4e8785d7d1ec8c4931
Sven Matzke <matzke.sven@googlemail.com>
2018-02-28 18:45:57 +0100

    changing data in main

--- Source/python/somemain.py
+++ Source/python/somemain.py
@@ -6,50 +6,36 @@ buf = bytearray(2048)
 import time
 import urequests
 import wlan
-from Source.externalpackages import userv
+import userv
 import sensor
 import settings
 import ujson
+import machine
+import ntptime
 
 # get config data
-print(gc.mem_free())
 loaded_settings = settings.get_settings()
 # if anything fails we are ready to set up
 restful_online_time = loaded_settings.get('awake_time_for_config', 300)
 keep_alive_time = restful_online_time
 
-
-# reset verhalten
-
-# der teil ist 700 kb verlust
 if wlan.sta_if.active():
+    # update time
+
     # wenn connected try to get new config if this fails we set restful_online_time
     # => this will result in more energy consumption, but else you can config this device
     request_url = loaded_settings.get("request_url", None)
     if request_url is not None:
         try:
-            # get new settings  # TODO frage ob das drin bleibt
-            parsed_reponse = urequests.get(request_url)
-            if int(parsed_reponse.get('status_code', 500)) >= 300:
-                raise ConnectionError("Response was incorrect")
-            elif "application/json" not in parsed_reponse['header'].get('Content-Type', ""):
-                raise ConnectionError("Mime type is not correct")
-            new_config = parsed_reponse.get("body", None)
-            if new_config is None or new_config == "":
-                raise NotADirectoryError("New config didnt had the proper data")
-            loaded_settings = settings.save_settings(
-                old_config=loaded_settings,
-                new_config=new_config
-            )
-
             # send plant_monitor data
             sensor_data = sensor.sensor_data()
             sensor_data.update(loaded_settings.get('added_infos_to_sensor_data', {}))
-            parsed_reponse = urequests.post(
+            response = urequests.post(
                 request_url,
-                body=ujson.dumps(sensor_data)
+                json=sensor_data
             )
-            if int(parsed_reponse.get('status_code', 500)) >= 300:
+
+            if int(response.status_code) >= 300:
                 raise ConnectionError("Response was incorrect")
 
             keep_alive_time = loaded_settings.get('keep_alive_time_s')
@@ -57,6 +43,10 @@ if wlan.sta_if.active():
         except Exception as e:
             # data was not send so we will need some config changes
             restful_online_time = loaded_settings.get('awake_time_for_config', 300)
+
+if machine.reset_cause() == machine.HARD_RESET:
+    restful_online_time = 300  # config time will be hard set
+
 
 # webserver will be started to listen
 # Need to clean up some cache before its too low
@@ -69,12 +59,12 @@ start_time = time.time()
 
 def shutdown_reached():
     return (time.time()-start_time) < 180
-    # global accumulated_time
-    # print("shutdown_active")
-    # while accumulated_time < restful_online_time:# and (time.time() - server.last_request_time) <= keep_alive_time:
-    #     accumulated_time += int(keep_alive_time)
-    # print("loop closes")
-    # loop.stop()
+    global accumulated_time
+    print("shutdown_active")
+    while accumulated_time < restful_online_time:# and (time.time() - server.last_request_time) <= keep_alive_time:
+        accumulated_time += int(keep_alive_time)
+    print("loop closes")
+    loop.stop()
 
 # declare pages
 
