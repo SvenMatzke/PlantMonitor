4ea83c2fd5296d9d6df018be0ac874242dba1e7d
Sven Matzke <matzke.sven@googlemail.com>
2018-03-01 22:20:43 +0100

    Hard reset now triggers creating new network

--- Source/python/boot.py
+++ Source/python/boot.py
@@ -3,7 +3,7 @@ import gc
 gc.collect()
 import wlan
 import settings
-
+import machine
 
 print("deactivate wlan")
 wlan.ap_if.active(False)
@@ -11,11 +11,14 @@ wlan.sta_if.active(False)
 
 config_data = settings.get_settings()
 wlan_config = config_data.get('wlan', {})
-
-print("connect existing network")
-network_connected = wlan.connect_to_existing_network(
-    essid=wlan_config.get('ssid'),
-    password=wlan_config.get('password'),
-)
-if not network_connected:
+# On Hardreset we want to configure on a fresh network
+if machine.reset_cause() != machine.HARD_RESET:
+    print("connect existing network")
+    network_connected = wlan.connect_to_existing_network(
+        essid=wlan_config.get('ssid'),
+        password=wlan_config.get('password'),
+    )
+    if not network_connected:
+        wlan.create_an_network()
+else:
     wlan.create_an_network()
